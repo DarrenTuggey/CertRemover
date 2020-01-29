2db865e952d98a043efadaa3050a1a867bc8fbf7
DarrenTuggey <54648574+DarrenTuggey@users.noreply.github.com>
2020-01-29 09:27:48 -0800

    Update CertRemover.cs
    Updated to write no cert was found and to keep the window open until a key is pressed

--- CertRemover.cs
+++ CertRemover.cs
@@ -3,24 +3,33 @@ using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.IO;
 
-namespace CertRemover
+namespace Cert
 {
-    class CertRemover
+    class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            X509Store store = new X509Store("My", StoreLocation.CurrentUser); 
+            X509Store store = new X509Store("My", StoreLocation.CurrentUser);
             store.Open(OpenFlags.ReadWrite);
-            X509Certificate2Collection storeCollection = (X509Certificate2Collection)store.Certificates;
-            foreach (X509Certificate2 x509 in storeCollection)
+            X509Certificate2 certificate = new X509Certificate2();
+            X509Certificate2Collection storecollection2 = (X509Certificate2Collection)store.Certificates;
+            int certCount = 0;
+            foreach (X509Certificate2 x509 in storecollection2)
             {
                 if(x509.FriendlyName.Contains("ID - "))
                 {
-                    Console.WriteLine("certificate name: {0}", x509.FriendlyName);
+                    certCount++;
+                    Console.WriteLine("Attempting to remove certificate: {0}", x509.FriendlyName);
                     store.Remove (x509);
                 }
             }
+            if (certCount == 0)
+            {
+                System.Console.WriteLine("No ID Cert found");
+            }
             store.Close();
+            System.Console.WriteLine("Press any key to close");
+            Console.ReadKey();
         }
     }
 }
